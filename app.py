839695394cf8c62a7d210ed1f6c8cf78d65a0d4d
RomanIlchenko1308 <romailch1308@gmail.com>
2022-09-27 19:35:59 +0200

    Init CSS styling for man grid view of Python Dash Plotly Robot App

--- app.py
+++ app.py
@@ -8,8 +8,26 @@ app = dash.Dash('')
 app.scripts.config.serve_locally = True
 
 app.layout = html.Div([
-    html.Div("Hello Dash App!", style={'color': 'blue', 'fontSize': 14})
-], style={'marginBottom': 50, 'marginTop': 25, "backgroundColor": "red"})
+    html.Div([
+        html.Img(src="/assets/img/robot_icon.png", className="robotLogo"),
+        html.H2("Robot App"),
+        html.A(children=[
+            html.Img(src="/assets/img/github_icon.svg", className="githubLogo")
+        ],
+               href="https://github.com/RomanIlchenko1308/PythonRobotApp",
+               className="githubLogo")
+    ], className="appGitHub"),
+    html.Div([
+        html.Button('Button 1', id='btn-nclicks-1', n_clicks=0),
+        html.Button('Button 2', id='btn-nclicks-2', n_clicks=0),
+        html.Button('Button 3', id='btn-nclicks-3', n_clicks=0),
+        html.Button('Button 4', id='btn-nclicks-4', n_clicks=0),
+        html.Button('Button 5', id='btn-nclicks-5', n_clicks=0),
+        html.Div(["Contact with me:"])
+    ], className="menuControlFlow"),
+    html.Div([], className="vtkVisualization"),
+    html.Div([], className="manualCommands"),
+], className="bodyDiv")
 
 if __name__ == '__main__':
     app.run_server(debug=True)
