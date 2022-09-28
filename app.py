402843432c170a085a7d7b25965504e4e1e320bb
RomanIlchenko1308 <romailch1308@gmail.com>
2022-09-28 15:49:53 +0200

    Add Icons to the buttons

--- app.py
+++ app.py
@@ -1,8 +1,32 @@
 import dash
 from dash import dcc
 from dash import html
+import dash_vtk
+from dash_vtk.utils import to_mesh_state
 
-app = dash.Dash('')
+try:
+    # VTK 9+
+    from vtkmodules.vtkImagingCore import vtkRTAnalyticSource
+except ImportError:
+    # VTK =< 8
+    from vtk.vtkImagingCore import vtkRTAnalyticSource
+
+# Use VTK to get some data
+data_source = vtkRTAnalyticSource()
+data_source.Update()  # <= Execute source to produce an output
+dataset = data_source.GetOutput()
+
+# Use helper to get a mesh structure that can be passed as-is to a Mesh
+# RTData is the name of the field
+mesh_state = to_mesh_state(dataset)
+
+content = dash_vtk.View([
+    dash_vtk.GeometryRepresentation([
+        dash_vtk.Mesh(state=mesh_state)
+    ]),
+])
+
+app = dash.Dash(__name__)
 
 app.scripts.config.serve_locally = True
 
@@ -20,34 +44,48 @@ app.layout = html.Div([
     html.Div([
         html.Div([
             html.Button(children=[
-                html.Div("Start", className="btnText"),
+                html.Div("Home", className="btnText"),
                 html.Div(children=[
-                    html.Div("GO!", className="btnText2")
+                    html.Div(
+                        children=[
+                            html.Img(src="/assets/img/robot_home_origin_icon.svg", className="robNavLogo")],
+                        className="btnText2")
                 ], className="btnTwo"),
             ], className="button", n_clicks=0),
-            html.Div(dcc.Input(id='input-on-submit button', type='text')),
             html.Button(children=[
                 html.Div("Place", className="btnText"),
                 html.Div(children=[
-                    html.Div("GO!", className="btnText2")
+                    html.Div(
+                        children=[
+                            html.Img(src="/assets/img/robot_place_icon.svg", className="robNavLogo")],
+                        className="btnText2")
                 ], className="btnTwo"),
             ], className="button", n_clicks=0),
             html.Button(children=[
                 html.Div("Left", className="btnText"),
                 html.Div(children=[
-                    html.Div("GO!", className="btnText2")
+                    html.Div(
+                        children=[
+                            html.Img(src="/assets/img/robot_turn_left_icon.svg", className="robNavLogo")],
+                        className="btnText2")
                 ], className="btnTwo"),
             ], className="button", n_clicks=0),
             html.Button(children=[
                 html.Div("Right", className="btnText"),
                 html.Div(children=[
-                    html.Div("GO!", className="btnText2")
+                    html.Div(
+                        children=[
+                            html.Img(src="/assets/img/robot_turn_right_icon.svg", className="robNavLogo")],
+                        className="btnText2")
                 ], className="btnTwo"),
             ], className="button", n_clicks=0),
             html.Button(children=[
                 html.Div("Report", className="btnText"),
                 html.Div(children=[
-                    html.Div("GO!", className="btnText2")
+                    html.Div(
+                        children=[
+                            html.Img(src="/assets/img/robot_report_icon.svg", className="robNavLogo")],
+                        className="btnText2")
                 ], className="btnTwo"),
             ], className="button", n_clicks=0),
         ], className="menuButtons menuButtonsBackground"),
@@ -62,7 +100,12 @@ app.layout = html.Div([
         ], className="contactWithMe menuButtonsBackground")
     ], className="menuControlFlow"),
 
-    html.Div([], className="vtkVisualization"),
+    html.Div([
+        html.Div(
+            style={"width": "100%", "height": "100%"},
+            children=[content],
+        )
+    ], className="vtkVisualization"),
     html.Div([], className="manualCommands"),
 ], className="bodyDiv")
 
