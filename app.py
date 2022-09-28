d2363d390de2859e1b5ac64aba75a309dff2aaa9
RomanIlchenko1308 <romailch1308@gmail.com>
2022-09-28 13:46:32 +0200

    Add Menu Buttons and Contacts CSS Design and Animation

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import dash
 from dash import dcc
 from dash import html
 
-
 app = dash.Dash('')
 
 app.scripts.config.serve_locally = True
@@ -12,19 +11,57 @@ app.layout = html.Div([
         html.Img(src="/assets/img/robot_icon.png", className="robotLogo"),
         html.H2("Robot App"),
         html.A(children=[
-            html.Img(src="/assets/img/github_icon.svg", className="githubLogo")
+            html.Img(src="/assets/img/Github_icon.svg", className="githubLogo")
         ],
-               href="https://github.com/RomanIlchenko1308/PythonRobotApp",
-               className="githubLogo")
-    ], className="appGitHub"),
+            href="https://github.com/RomanIlchenko1308/PythonRobotApp",
+            target="_blank",
+            className="githubLogo")
+    ], className="projectHeader"),
     html.Div([
-        html.Button('Button 1', id='btn-nclicks-1', n_clicks=0),
-        html.Button('Button 2', id='btn-nclicks-2', n_clicks=0),
-        html.Button('Button 3', id='btn-nclicks-3', n_clicks=0),
-        html.Button('Button 4', id='btn-nclicks-4', n_clicks=0),
-        html.Button('Button 5', id='btn-nclicks-5', n_clicks=0),
-        html.Div(["Contact with me:"])
+        html.Div([
+            html.Button(children=[
+                html.Div("Start", className="btnText"),
+                html.Div(children=[
+                    html.Div("GO!", className="btnText2")
+                ], className="btnTwo"),
+            ], className="button", n_clicks=0),
+            html.Div(dcc.Input(id='input-on-submit button', type='text')),
+            html.Button(children=[
+                html.Div("Place", className="btnText"),
+                html.Div(children=[
+                    html.Div("GO!", className="btnText2")
+                ], className="btnTwo"),
+            ], className="button", n_clicks=0),
+            html.Button(children=[
+                html.Div("Left", className="btnText"),
+                html.Div(children=[
+                    html.Div("GO!", className="btnText2")
+                ], className="btnTwo"),
+            ], className="button", n_clicks=0),
+            html.Button(children=[
+                html.Div("Right", className="btnText"),
+                html.Div(children=[
+                    html.Div("GO!", className="btnText2")
+                ], className="btnTwo"),
+            ], className="button", n_clicks=0),
+            html.Button(children=[
+                html.Div("Report", className="btnText"),
+                html.Div(children=[
+                    html.Div("GO!", className="btnText2")
+                ], className="btnTwo"),
+            ], className="button", n_clicks=0),
+        ], className="menuButtons menuButtonsBackground"),
+        html.Div([
+            html.H4("Contact with me:"),
+            html.A(children=[
+                html.Img(src="/assets/img/LinkedIn_icon.svg", className="githubLogo")
+            ],
+                href="https://www.linkedin.com/in/romanilichenko/",
+                target="_blank",
+                className="linkedInLogo")
+        ], className="contactWithMe menuButtonsBackground")
     ], className="menuControlFlow"),
+
     html.Div([], className="vtkVisualization"),
     html.Div([], className="manualCommands"),
 ], className="bodyDiv")
